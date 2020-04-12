4ece96b8bb3dec8820f268fb52d5ae9c30e87b64
niedermansam <niederman.sam@gmail.com>
2020-04-12 17:47:02 -0600

    Updates

---
+++ precache-manifest.077ab8409740e921af779f180dd30028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d9f7c79a461403627c04f3a78593d1",
+    "url": "/covid-dashboard/index.html"
+  },
+  {
+    "revision": "7f7e29660b1dbf070a10",
+    "url": "/covid-dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3af596560e1fdae11daf",
+    "url": "/covid-dashboard/static/js/2.d8817767.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-dashboard/static/js/2.d8817767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7e29660b1dbf070a10",
+    "url": "/covid-dashboard/static/js/main.9fa96ce5.chunk.js"
+  },
+  {
+    "revision": "76f914ece785af55556e",
+    "url": "/covid-dashboard/static/js/runtime-main.2e9a8ba7.js"
+  }
+]);
